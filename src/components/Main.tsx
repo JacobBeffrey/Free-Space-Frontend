e47857b50f1009f25066b02bad419e7381f36c69
earfworm <deshawnreid2021@gmail.com>
2022-12-02 22:51:45 -0500

    APOD API

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import PictureOfTheDay from "../models/PictureOfTheDay";
+import { getPictureOfTheDay } from "../services/PictureOfTheDayApiService";
 import "./Main.css";
 
 const Main = () => {
-  return <div className="Main"></div>;
+  const [apod, setApod] = useState<PictureOfTheDay>();
+  useEffect(() => {
+    getPictureOfTheDay().then((res) => setApod(res));
+  }, []);
+
+  return (
+    <div className="Main">
+      <h2>{`${apod?.title}`}</h2>
+      <p>{`${apod?.date}`}</p>
+      <img src={`${apod?.hdurl}`} alt={`${apod?.title}`} />
+      <p>{`${apod?.explanation}`}</p>
+    </div>
+  );
 };
 
 export default Main;

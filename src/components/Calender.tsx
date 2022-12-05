aaca95b9bcc6c86b0946e5522268f390b423b371
Jacob Beffrey <jacob.beffrey@Gmail.com>
2022-12-05 13:20:18 -0500

    setting up the bones from the calendar

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+import CalendarEvent from "../models/CalendarEvent";
+import { getAllCalendarEvent } from "../services/CalendarEventsApiService";
 import "./Calendar.css";
 
 const Calendar = () => {
-  return <div className="Calendar">Calendar works</div>;
+  const [calendarInfo, setCalendarInfo] = useState<CalendarEvent[]>();
+
+  useEffect(() => {
+    getAllCalendarEvent().then((res) => console.log(res));
+  }, []);
+  return <div className="Calendar"></div>;
 };
 
 export default Calendar;
--- src/services/CalendarEventsApiService.ts
+++ src/services/CalendarEventsApiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import CalendarEvent from "../models/CalendarEvent";
 
-const baseUrl: string = process.env.REACT_APP_API_BASE_URL || "";
+const baseUrl: string = process.env.REACT_APP_API_URL || "";
 export const getAllCalendarEvent = (): Promise<CalendarEvent[]> => {
-  return axios.get(`${baseUrl}/free_space`).then((res) => res.data);
+  return axios.get(`${baseUrl}/calendar/all-events`).then((res) => res.data);
 };

c781d6f0b8f3880282915a867b042f3c3f70d70d
earfworm <deshawnreid2021@gmail.com>
2022-12-07 15:31:26 -0500

    Account sign in

--- src/services/AccountInfoApiService.ts
+++ src/services/AccountInfoApiService.ts
@@ -2,6 +2,14 @@ import axios from "axios";
 import AccountInfo from "../models/AccountInfo";
 
 const baseUrl: string = process.env.REACT_APP_API_URL || "";
-export const getAllAccountInfo = (): Promise<AccountInfo[]> => {
-  return axios.get(`${baseUrl}/AccountInfo/all-login`).then((res) => res.data);
+export const getAccountInfo = (uid: string): Promise<AccountInfo> => {
+  return axios.get(`${baseUrl}/AccountInfo/${uid}`).then((res) => res.data);
+};
+
+export const addNewAccount = (
+  newAccount: AccountInfo
+): Promise<AccountInfo> => {
+  return axios
+    .post(`${baseUrl}/AccountInfo`, newAccount)
+    .then((res) => res.data);
 };

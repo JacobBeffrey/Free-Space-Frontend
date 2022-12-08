e83b58078e344b33795027d5dda5d65c2c7a381b
earfworm <deshawnreid2021@gmail.com>
2022-12-08 01:14:17 -0500

    Put events with calendar cells

--- src/components/EventsCalendar.tsx
+++ src/components/EventsCalendar.tsx
@@ -9,6 +9,8 @@ import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import enUS from "date-fns/locale/en-US";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 import DisplayEventInfo from "../models/DisplayEventInfo";
 
 const locales = {
@@ -61,7 +63,8 @@ const EventsCalendar = () => {
       <Calendar
         localizer={localizer}
         events={calendarJsonInput}
-        // startAccessor="dtstart"
+        startAccessor="start"
+        endAccessor="end"
         style={{ height: 500 }}
       />
     </div>
